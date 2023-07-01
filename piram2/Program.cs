1767eecbaec464a140aacb439d9c90deff73124b
neomachine <neomachinemail@gmail.com>
2023-07-01 11:17:21 -0500

    first commit

---
+++ piram2/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace piram2
+{
+    internal class Program
+    {
+        //NEO-mach
+        static void Main(string[] args)
+        {
+            Soser obj = new Soser();
+            obj.Impre();
+        }
+
+    }
+}
---
+++ piram2/Soser.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace piram2
+{
+    internal class Soser
+    {
+        //NEO-mach
+        public void Impre()
+        {
+
+            const int ocho = 8;
+            long salida;
+            long a = 1;
+            const int b = 10;
+            int c = 2;
+            int espacio = 10;
+            for (int i = 1; i <= 9; i++)
+            {
+                salida = a * ocho + i;
+                Console.SetCursorPosition(espacio, i);
+                Console.WriteLine("{0} x {1} + {2} = {3}", a, ocho, i, salida);
+                a = a * b + c;
+                c += 1;
+                espacio -= 1;
+
+            }
+            Console.WriteLine("             NEO-mach");
+            Console.ReadKey();
+        }
+    }
+}
